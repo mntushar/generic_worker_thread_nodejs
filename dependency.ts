76a9502a3130b5afb46152aa2e6f4ec5e8163a9d
M N Tushar <45463161+mntushar@users.noreply.github.com>
2025-10-12 18:22:43 +0600

    Create dependency.ts

---
+++ dependency.ts
@@ -0,0 +1,8 @@
+// Your dependency functions
+export const doSomeThing = (sum: number) => {
+  return sum * 2; // Example transformation
+};
+
+export const anotherHelper = (value: number) => {
+  return value + 1000;
+};
