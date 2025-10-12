862454cf56ebc1adddb5709caa86503b5862a521
M N Tushar <45463161+mntushar@users.noreply.github.com>
2025-10-12 18:22:13 +0600

    Update Example for dependency

--- Example.ts
+++ Example.ts
@@ -1,13 +1,37 @@
 import { WorkerPool } from "./worker_thread";
+import { anotherHelper, doSomeThing } from "./dependency";
 
 const size = 1e9;
 const pool = new WorkerPool();
 
-result = await pool.executeTask<number>(
-    (data: number) => {
-        let sum = 0;
-        for (let i = 0; i < data; i++) sum += i;
-        return sum;
-    },
-    [size]
-);
+// example without dependency
+(async () => {
+    result = await pool.executeTask<number>(
+        (data: number) => {
+            let sum = 0;
+            for (let i = 0; i < data; i++) sum += i;
+            return sum;
+        },
+        [size]
+    );
+    console.log("Worker result:", result);
+}).();
+
+
+// example with dependency
+(async () => {
+    const size = 1e9;
+    const pool = new WorkerPool();
+    const result = await pool.executeTask<number>(
+        (doSomeThing, anotherHelper, data) => {
+            let sum = 0;
+            for (let i = 0; i < data; i++) sum += i;
+            const processed = doSomeThing(sum);
+            return anotherHelper(processed);
+        },
+        [size], // Regular arguments
+        { doSomeThing, anotherHelper } // Dependencies to inject
+    );
+
+    console.log("Worker result:", result);
+})();
