272b1da33eee3c3f7b4f711d98f4a118bec748ec
M N Tushar <45463161+mntushar@users.noreply.github.com>
2025-10-13 16:55:22 +0600

    add  module dependency in example

--- Example.ts
+++ Example.ts
@@ -1,22 +1,21 @@
-import { WorkerPool } from "./worker_thread";
 import { anotherHelper, doSomeThing } from "./dependency";
-
-const size = 1e9;
-const pool = new WorkerPool();
+import { WorkerPool } from "./worker";
 
 // example without dependency
 (async () => {
-    result = await pool.executeTask<number>(
+    const size = 1e9;
+    const pool = new WorkerPool();
+    const result = await pool.executeTask<number>(
         (data: number) => {
             let sum = 0;
             for (let i = 0; i < data; i++) sum += i;
             return sum;
         },
+        {},
         [size]
     );
     console.log("Worker result:", result);
-}).();
-
+})();
 
 // example with dependency
 (async () => {
@@ -29,8 +28,33 @@ const pool = new WorkerPool();
             const processed = doSomeThing(sum);
             return anotherHelper(processed);
         },
+        { doSomeThing, anotherHelper }, // Dependencies to inject
         [size], // Regular arguments
-        { doSomeThing, anotherHelper } // Dependencies to inject
+    );
+
+    console.log("Worker result:", result);
+})();
+
+// example with module dependency
+(async () => {
+    const size = 1e9;
+    const pool = new WorkerPool();
+
+    const result = await pool.executeTask<number>(
+        (fs, doSomeThing, anotherHelper, data) => {
+            return new Promise((resolve, reject) => {
+                let sum = 0;
+                for (let i = 0; i < data; i++) sum += i;
+                const processed = doSomeThing(sum);
+                sum = anotherHelper(processed);
+
+                fs.writeFileSync('result.txt', `Worker result: ${sum}\n`, 'utf8');
+
+                resolve(sum);
+            });
+        },
+        { fs: 'fs', doSomeThing, anotherHelper }, // Dependencies to inject
+        [size], // Regular arguments
     );
 
     console.log("Worker result:", result);
