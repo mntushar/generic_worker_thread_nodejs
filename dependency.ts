bd523e97469bc8a37524d648bd6bc8b09a7cd32a
M N Tushar <45463161+mntushar@users.noreply.github.com>
2025-10-27 11:26:39 +0600

    Delete dependency.ts

--- dependency.ts
+++
@@ -1,8 +0,0 @@
-// Your dependency functions
-export const doSomeThing = (sum: number) => {
-  return sum * 2; // Example transformation
-};
-
-export const anotherHelper = (value: number) => {
-  return value + 1000;
-};
